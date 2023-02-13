c0d6bfd2fc76c71447a92a51ceb8376d5ffc52ff
VoDracseck <79907142+Blazonier@users.noreply.github.com>
2023-02-13 11:14:48 +0100

    xd

---
+++ saves/my_epic_save.py
@@ -0,0 +1,20 @@
+
+global save_name
+global player_stats
+global inventory
+global hasBackpack
+global backpack
+
+save_name = 'my epic save'
+
+player_stats = {
+    'hp': 3,
+    'pos': [100, 100]
+}
+inventory = [
+    '', '', '', '', '', '', '', '', '', '', '', ''
+]
+hasBackpack = False
+backpack = [
+    '', '', '', '', '', ''
+]
